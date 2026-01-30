7d6517d5bea8b21939a5cb7a4aeb85c6db85b8d4
Anthony Orji <ocanthony4real@gmail.com>
2026-01-30 16:38:01 +0100

    refined docker

--- llm_engineering/__init__.py
+++ llm_engineering/__init__.py
@@ -1,4 +1,4 @@
-from llm_engineering import application, domain, infrastructure
+from llm_engineering import application, domain, infrastructure, model
 from llm_engineering.settings import settings
 
-__all__ = ["settings", "application", "domain", "infrastructure"]
+__all__ = ["settings", "application", "domain", "infrastructure", "model"]
